7a57dbbddc89e0bf3224c1bf30d8c37161bdc6ec
KsenyaNikitchenko <113337726+KsenyaNikitchenko@users.noreply.github.com>
2024-02-12 19:26:51 +0300

    Добавлено задание 7
    11. Дана строка. Необходимо найти все незадействованные символы латиницы в этой строке.

--- task6-8.py
+++ task6-8.py
@@ -14,6 +14,28 @@ def kolnums(s):
 s=input("Введите строку: ")
 print("Количество чисел в строке, которые меньше 5, равно ",kolnums(s))
 
+print("11. Дана строка. Необходимо найти все незадействованные символы латиницы в этой строке.")
+
+def notabc(s):
+    abc=[]
+    n=0
+    for i in s:
+        if(i.isalpha() and abc.count(i.lower())==0):
+            abc.append(i.lower())
+    abc.sort()
+    nots=[]
+    if(len(abc)!=26):
+        for i in range(97,123,+1):
+            if(abc.count(chr(i))!=1):
+                n+=1
+                nots.append(chr(i))
+    return nots
+
+s=input("Введите строку: ")
+nots=notabc(s)
+print("В данной строке отсутствует ",len(nots)," символов латинского алфавита")
+print(nots)
+
 print("15. Дана строка. Необходимо подсчитать количество цифр в этой строке, значение которых больше 5.")
 
 def kolnums5(s):
