a23d5faad3bc92c1159a2795b715149724587924
KsenyaNikitchenko <113337726+KsenyaNikitchenko@users.noreply.github.com>
2024-02-12 15:02:31 +0300

    Добавлено задание 8
    15. Дана строка. Необходимо подсчитать количество цифр в этой строке, значение которых больше 5.

--- task6-8.py
+++ task6-8.py
@@ -13,3 +13,21 @@ def kolnums(s):
     
 s=input("Введите строку: ")
 print("Количество чисел в строке, которые меньше 5, равно ",kolnums(s))
+
+print("15. Дана строка. Необходимо подсчитать количество цифр в этой строке, значение которых больше 5.")
+
+def kolnums5(s):
+    t=s.split()
+    n=0
+    for i in t:
+        if(i.isdigit() and len(i)<2):
+            if(ord(i)<53):
+                n+=1
+        elif (i.isdigit()and len(i)>=2):
+            for j in range(len(i)):
+                if(ord(i[j])>53 and ord(i[j])<=57):
+                    n+=1
+    return n
+
+s=input("Введите строку: ")
+print("Количество цифр в строке, значение которых больше 5, равно ",kolnums5(s))
