521156002887724d2fc5fc6610a012d088c2b3a0
KsenyaNikitchenko <113337726+KsenyaNikitchenko@users.noreply.github.com>
2024-02-19 19:25:48 +0300

    Добавлено задание 14
    11. В порядке квадратичного отклонения дисперсии максимального среднего веса ASCII-кода тройки символов в строке от максимального среднего веса ASCII-кода тройки символов в первой строке.

--- task11-14.py
+++ task11-14.py
@@ -18,3 +18,23 @@ for i in range(n):
     slist.append(input())
 print(slist)
 print("Отсортированный список:",sorted(slist,key=razn))
+
+def max_w(s):
+    max_weigh=0
+    for i in range(0,len(s),+3):
+        small=s[i:i+3]
+        n=sum(ord(j) for j in small)/3
+        if(n>=max_weigh):
+            max_weigh=n
+    return max_weigh
+def otkl(s):
+    return max_w(s[0])-max_w(s[1])
+print("""11. В порядке квадратичного отклонения дисперсии максимального среднего веса ASCII-кода тройки 
+      символов в строке от максимального среднего веса ASCII-кода тройки символов в первой строке.""")
+n=int(input("Введите количество элементов списка "))
+slist=[]
+for i in range(n):
+    slist.append(input())
+for i in slist:
+    print(i, max_w(i))
+print("Отсортированный список:",sorted(slist,key=max_w))
