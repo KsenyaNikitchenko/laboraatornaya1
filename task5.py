3a893e1bd9dfeb10e1dcfa76e28d211749809b80
KsenyaNikitchenko <113337726+KsenyaNikitchenko@users.noreply.github.com>
2024-02-26 16:34:04 +0300

    Исправлено задание 5

--- task5.py
+++ task5.py
@@ -1,12 +1,16 @@
+def date_in_string(string):
+    months=['января','февраля','марта','арпеля','мая','июня','июля','августа','сентября','октября','ноября','декабря']
+    slist=list(string.split())
+    datelist=[]
+    for i in months:
+        if (i in slist):
+            for j in range(slist.count(i)):
+                n=slist.index(i)
+                if(slist[n-1].isdigit() and slist[n+1].isdigit()):
+                    date=[slist[n-1],slist[n],slist[n+1]]
+                    datelist.append(" ".join(date))
+    return datelist
+
 print("Задание 5. Дана строка. Необходимо найти все даты, которые описаны в виде '31 февраля 2007'.")
-s=input("Введите строку: ")
-months=['января','февраля','марта','арпеля','мая','июня','июля','августа','сентября','октября','ноября','декабря']
-slist=list(s.split())
-datelist=[]
-for i in months:
-    if (i in slist):
-        for j in range(slist.count(i)):
-            n=slist.index(i)
-            date=[slist[n-1],slist[n],slist[n+1]]
-            datelist.append(" ".join(date))
-print("Список дат ",datelist)
+listofdates=input("Введите строку: ")
+print("Список дат: ",date_in_string(listofdates))
